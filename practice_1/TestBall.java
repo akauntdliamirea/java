3ab25d0aab70186fb2083f2c3512fb1491b6ca51
akauntdliamirea <154614774+akauntdliamirea@users.noreply.github.com>
2023-12-22 18:37:24 +0300

    Add files via upload

---
+++ practice_1/TestBall.java
@@ -0,0 +1,13 @@
+package practice_1;
+public class TestBall {
+    public static void main(String[] args) {
+        Ball ball1 = new Ball("red");
+        Ball ball2 = new Ball("blue");
+
+        System.out.println(ball1);
+        System.out.println(ball2.toString());
+
+        ball1.setColor("green");
+        System.out.println(ball1.toString());
+    }
+}
---
+++ practice_1/TestBook.java
@@ -0,0 +1,13 @@
+package practice_1;
+public class TestBook {
+    public static void main(String[] args) {
+        Book book1 = new Book("Rich dad, poor dad", "Robert Kiyosaki");
+        Book book2 = new Book("Grok algorithms", "Aditya Bhargavam algorithms");
+
+        System.out.println(book1.toString());
+        System.out.println(book2.toString());
+
+        book1.setAuthor("J.D. Salinger");
+        System.out.println(book1.toString());
+    }
+}
